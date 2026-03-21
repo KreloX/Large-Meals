afb80d5bc7333a7a0fb2ec7a76af80880378d484
KreloX <krelox@seznam.cz>
2026-03-21 12:45:50 +0100

    Tag food items

--- src/main/java/cz/krelox/large_meals/data/LMItemTagsProvider.java
+++ src/main/java/cz/krelox/large_meals/data/LMItemTagsProvider.java
@@ -1,7 +1,6 @@
 package cz.krelox.large_meals.data;
 
 import cz.krelox.large_meals.LargeMeals;
-import cz.krelox.large_meals.item.LMItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
@@ -9,8 +8,11 @@ import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
+import vectorwing.farmersdelight.common.tag.ModTags;
 
 import java.util.concurrent.CompletableFuture;
+
+import static cz.krelox.large_meals.item.LMItems.*;
 
 public class LMItemTagsProvider extends ItemTagsProvider {
     public LMItemTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
@@ -19,6 +21,12 @@ public class LMItemTagsProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(LMItems.OMURICE_BLOCK.get(), LMItems.MUSHROOM_POT_PIE_BLOCK.get(), LMItems.ROASTED_MUTTON_RACK_BLOCK.get());
+        tag(ModTags.MEALS).add(COD_DELUXE.get(), HEARTY_LUNCH.get(), CHICKEN_CURRY.get(), PASTA_WITH_MUSHROOM_SAUCE.get());
+        tag(ModTags.SWEETS).add(SWEET_BERRY_CUSTARD.get(), RICE_PUDDING.get());
+        tag(ModTags.FEASTS).add(OMURICE_BLOCK.get(), MUSHROOM_POT_PIE_BLOCK.get(), ROASTED_MUTTON_RACK_BLOCK.get());
+
+        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(OMURICE_BLOCK.get(), MUSHROOM_POT_PIE_BLOCK.get(), ROASTED_MUTTON_RACK_BLOCK.get());
+        tag(Tags.Items.FOODS_SOUP).add(PUFFERFISH_BROTH.get(), POTATO_SOUP.get(), RED_SOUP.get(), TOMATO_EGG_SOUP.get());
+        tag(Tags.Items.FOODS_PIE).add(MUSHROOM_POT_PIE.get());
     }
 }
